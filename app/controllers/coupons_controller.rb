ddef656de3be7952cab81ee9002bf0e79fe72e8c
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 01:40:55 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -8,31 +8,27 @@ class CouponsController < ApplicationController
 
   def create
     @coupon = Coupon.new
-   @coupon[:coupon_code] = params[:coupon][:coupon_code]
-   @coupon[:store] = params[:coupon][:store]
-   @coupon.save
-   redirect_to coupon_path(@coupon)
- end
-  #   @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
-  # #   @coupon = Coupon.new(coupon_params)
-  #   # @coupon.save
-  #   redirect_to coupon_path(@coupon)
-  #   # else
-  #   #   render "new"
-  #   # end
-  # end
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
 
-#   private
-#   # Using a private method to encapsulate the permissible parameters is
-#   # a good pattern since you'll be able to reuse the same permit
-#
-#   def coupon_params
-#     params.require(:coupon).permit(:coupon_code, :store)
-#   end
-#
+    if @coupon.save
+      redirect_to coupon_path(@coupon)
+    else
+      render "new"
+    end
+  end
 
   def show
     @coupon = Coupon.find(params[:id])
   end
+
+  #   private
+  #   # Using a private method to encapsulate the permissible parameters is
+  #   # a good pattern since you'll be able to reuse the same permit
+  #
+  #   def coupon_params
+  #     params.require(:coupon).permit(:coupon_code, :store)
+  #   end
+  #
 
 end
